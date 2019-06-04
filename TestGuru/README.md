a906068e2b6103c382b1d9ff2a8aa5aa82054935
rysa <rysinda4@gmail.com>
2019-06-04 23:53:24 +0300

    2

--- TestGuru/README.md
+++ TestGuru/README.md
@@ -5,7 +5,8 @@ application up and running.
 
 Things you may want to cover:
 
-* Ruby version
+Ruby version 2.4.5
+At the moment this is all I can say
 
 * System dependencies
 
--- TestGuru/config/application.rb
+++ TestGuru/config/application.rb
@@ -10,6 +10,8 @@ module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.timezone UTC+3:00
+    config.location 'Spb'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
