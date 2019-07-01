31331dd51504dbb351b0ecf0a97d6820388be539
rysa <rysinda4@gmail.com>
2019-07-01 10:24:19 +0300

    .

--- TestGuru/app/models/test.rb
+++ TestGuru/app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
 
   def find_by_category(category)
-    Test.joins('JOIN categories ON categories.id = tests.category_id').
+  Test.joins('JOIN categories ON categories.id = tests.category_id').
   where(categories: { title: category }).
   order(title: :desc).
   pluck(:title)
