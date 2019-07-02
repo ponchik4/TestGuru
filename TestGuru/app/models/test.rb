61f6e10048ebdb1381a491af44be03786f56b3f1
rysa <rysinda4@gmail.com>
2019-07-02 20:59:11 +0300

    lesson 5 fix

--- TestGuru/app/models/test.rb
+++ TestGuru/app/models/test.rb
@@ -1,9 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category, class_name: "author"
   has_many :passed_tests
-  has_and_belongs_to_many :users, through: :passed_tests
+  has_many :users, through: :passed_tests
   has_many :questions
-
 
   def find_by_category(category)
   Test.joins('JOIN categories ON categories.id = tests.category_id').
