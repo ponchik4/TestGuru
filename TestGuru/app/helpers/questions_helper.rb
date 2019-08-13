73329847a5c5243dfc90688626fedc60aafb32da
rysa <rysinda4@gmail.com>
2019-08-13 18:49:25 +0300

    Lesson8

--- TestGuru/app/helpers/questions_helper.rb
+++ TestGuru/app/helpers/questions_helper.rb
@@ -1,2 +1,8 @@
 module QuestionsHelper
+  def question_header(test)
+    case params[:action]
+    when 'new' then "Create New #{test.title} question"
+    when 'edit' then "Edit #{test.title} question"
+    end
+  end
 end
