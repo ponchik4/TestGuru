a58bd4247b8c9ea596bb1d553b8309d2f6c21aac
rysa <rysinda4@gmail.com>
2019-08-13 19:26:04 +0300

    Lesson8

--- TestGuru/app/helpers/questions_helper.rb
+++ TestGuru/app/helpers/questions_helper.rb
@@ -1,8 +1,10 @@
 module QuestionsHelper
-  def question_header(test)
-    case params[:action]
-    when 'new' then "Create New #{test.title} question"
-    when 'edit' then "Edit #{test.title} question"
+  def question_header(question)
+    test_title = question.test_title
+    if question.persisted?
+      "Edit #{test_title} question"
+    else
+      "Create New #{test_title} question"
     end
   end
 end
