884154a4e3b3624a404eded104aad09a0a44ed9b
rysa <rysinda4@gmail.com>
2019-08-20 17:58:01 +0300

    something

---
+++ TestGuru/app/views/test_passages/_test_passage.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! test_passage, :id, :body, :created_at, :updated_at
+json.url test_passage_url(test_passage, format: :json)
--- TestGuru/config/routes.rb
+++ TestGuru/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :test_passages
+  resources :answers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tests#index'
 
