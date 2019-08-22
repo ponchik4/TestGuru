34bc2c70e8ece313574feef058a2e7b9f3f1b010
rysa <rysinda4@gmail.com>
2019-08-22 17:52:06 +0300

    lesson9

--- TestGuru/app/helpers/test_passages_helper.rb
+++ TestGuru/app/helpers/test_passages_helper.rb
@@ -1,2 +1,17 @@
 module TestPassagesHelper
+  def test_result(test_passage)
+   if test_passage.success_test?
+     puts 'test_success'
+   else
+     puts 'test_failed'
+   end
+ end
+
+ def color(test_passage)
+   if test_passage.success_test?
+     'block_green'
+   else
+     'block_red'
+   end
+ end
 end
